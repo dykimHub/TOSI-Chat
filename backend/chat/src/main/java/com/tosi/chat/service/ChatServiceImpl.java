ed435766ac69d5406522b1e5f9188ef3933fa4b8
김다윤 <dy.jo.kim@gmail.com>
2024-09-25 00:29:00 +0900

    Fix: 서비스에서 중복되는 부분 추출

--- backend/chat/src/main/java/com/tosi/chat/service/ChatServiceImpl.java
+++ backend/chat/src/main/java/com/tosi/chat/service/ChatServiceImpl.java
@@ -21,6 +21,8 @@ import java.util.List;
 @RequiredArgsConstructor
 @Service
 public class ChatServiceImpl implements ChatService {
+    private static final String ROLE_USER = "user";
+    private static final String ROLE_SYSTEM = "system";
     private final OpenAIProperties openAIProperties;
     private final RestTemplate restTemplate;
     @Value("${service.tale.url}")
@@ -35,21 +37,11 @@ public class ChatServiceImpl implements ChatService {
      */
     @Override
     public List<MultiChatMessage> sendInitChat(ChatInitInfoDto chatInitInfoDto) {
-        // 채팅 메시지 리스트 초기화
-        List<MultiChatMessage> multiChatMessagesList = new ArrayList<>();
-
+        List<MultiChatMessage> multiChatMessageList = new ArrayList<>();
         // 프롬프트 생성 및 사용자 메시지 추가
-        String initPrompt = this.makeChatInitRequestDto(chatInitInfoDto);
-        multiChatMessagesList.add(new MultiChatMessage("user", initPrompt));
-
-        // 채팅 메시지 리스트를 기반으로 OpenAI API 요청 객체 생성
-        MultiChatRequest multiChatRequest = this.makeMultiChatRequest(multiChatMessagesList);
-
-        // OpenAI API 응답 처리 및 시스템 메시지 추가
-        MultiChatResponse multiChatResponse = this.getResponse(this.buildHttpEntity(multiChatRequest));
-        multiChatMessagesList.add(new MultiChatMessage("system", multiChatResponse.getChoices().get(0).getMessage().getContent()));
-
-        return multiChatMessagesList;
+        String initPrompt = this.makeChatInitRequestDto(chatInitInfoDto).getInitPrompt();
+        multiChatMessageList.add(new MultiChatMessage(ROLE_SYSTEM, initPrompt));
+        return processChatRequest(multiChatMessageList);
     }
 
     /**
@@ -61,18 +53,10 @@ public class ChatServiceImpl implements ChatService {
      */
     @Override
     public List<MultiChatMessage> sendChat(ChatRequestDto chatRequestDto) {
-        // 이전 채팅 메시지를 사용하여 새 채팅 메시지 리스트 초기화
+        // 이전 채팅 메시지를 사용하여 리스트 초기화 및 새로운 사용자 메시지 추가
         List<MultiChatMessage> updatedMultiChatMessageList = new ArrayList<>(chatRequestDto.getPreMultiChatMessageList());
-
-        // 채팅 메시지 리스트를 기반으로 OpenAI API 요청 객체 생성
-        updatedMultiChatMessageList.add(new MultiChatMessage("user", chatRequestDto.getMultiChatMessage()));
-        MultiChatRequest multiChatRequest = this.makeMultiChatRequest(updatedMultiChatMessageList);
-
-        // OpenAI API 응답 처리 및 시스템 메시지 추가
-        MultiChatResponse multiChatResponse = this.getResponse(this.buildHttpEntity(multiChatRequest));
-        updatedMultiChatMessageList.add(new MultiChatMessage("system", multiChatResponse.getChoices().get(0).getMessage().getContent()));
-
-        return updatedMultiChatMessageList;
+        updatedMultiChatMessageList.add(new MultiChatMessage(ROLE_USER, chatRequestDto.getMultiChatMessage()));
+        return processChatRequest(updatedMultiChatMessageList);
     }
 
     /**
@@ -84,16 +68,12 @@ public class ChatServiceImpl implements ChatService {
      */
     @Override
     public List<MultiChatMessage> sendFinalChat(ChatRequestDto chatRequestDto) {
+        // 이전 채팅 메시지를 사용하여 새 채팅 메시지 리스트 초기화
         List<MultiChatMessage> updatedMultiChatMessageList = new ArrayList<>(chatRequestDto.getPreMultiChatMessageList());
-
-        String finalPrompt = chatRequestDto.getMultiChatMessage() + ChatFinalRequestDto.finalPrompt;
-        updatedMultiChatMessageList.add(new MultiChatMessage("user", finalPrompt));
-        MultiChatRequest multiChatRequest = this.makeMultiChatRequest(updatedMultiChatMessageList);
-
-        MultiChatResponse multiChatResponse = this.getResponse(this.buildHttpEntity(multiChatRequest));
-        updatedMultiChatMessageList.add(new MultiChatMessage("system", multiChatResponse.getChoices().get(0).getMessage().getContent()));
-
-        return updatedMultiChatMessageList;
+        // 새로운 사용자 메시지와 마지막 인사 프롬프트 추가
+        updatedMultiChatMessageList.add(new MultiChatMessage(ROLE_USER, chatRequestDto.getMultiChatMessage()));
+        updatedMultiChatMessageList.add(new MultiChatMessage(ROLE_SYSTEM, ChatFinalRequestDto.finalPrompt));
+        return processChatRequest(updatedMultiChatMessageList);
     }
 
     /**
@@ -102,7 +82,7 @@ public class ChatServiceImpl implements ChatService {
      * @param chatInitInfoDto 채팅을 시작할 때 필요한 학습 정보가 담긴 ChatInitInfoDto 객체
      * @return ChatInitInfoDto를 기반으로 생성된 프롬프트 문자열
      */
-    private String makeChatInitRequestDto(ChatInitInfoDto chatInitInfoDto) {
+    private ChatInitRequestDto makeChatInitRequestDto(ChatInitInfoDto chatInitInfoDto) {
         TaleDetailDto taleDetailDto = restTemplate.getForObject(taleURL + "content/" + chatInitInfoDto.getTaleId(), TaleDetailDto.class);
 
         return ChatInitRequestDto.builder()
@@ -110,20 +90,42 @@ public class ChatServiceImpl implements ChatService {
                 .characterName(chatInitInfoDto.getCharacterName())
                 .taleTitle(taleDetailDto.getTitle())
                 .taleContent(taleDetailDto.getContent())
-                .build()
-                .getInitPrompt();
+                .build();
     }
+
+    /**
+     * MultiChatRequest 객체를 생성하고 이를 감싼 HttpEntity 객체로 OpenAI API에 요청을 보냅니다.
+     * 응답받은 시스템 메시지를 기존 채팅 메시지 리스트에 추가한 후 업데이트된 리스트를 반환합니다.
+     *
+     * @param multiChatMessageList 사용자와 시스템 간의 기존 채팅 메시지 리스트
+     * @return OpenAI API 응답을 반영한 업데이트된 채팅 메시지 리스트
+     */
+    private List<MultiChatMessage> processChatRequest(List<MultiChatMessage> multiChatMessageList) {
+        // MultiChatRequest 객체 생성
+        MultiChatRequest multiChatRequest = makeMultiChatRequest(multiChatMessageList);
+
+        // OpenAI API에 요청 보내고 받은 응답을 추가
+        MultiChatResponse multiChatResponse = restTemplate.postForEntity(
+                        openAIProperties.getApiURL(),
+                        buildHttpEntity(multiChatRequest),
+                        MultiChatResponse.class)
+                .getBody();
+        multiChatMessageList.add(new MultiChatMessage(ROLE_SYSTEM, multiChatResponse.getChoices().get(0).getMessage().getContent()));
+        
+        return multiChatMessageList;
+    }
+
 
     /**
      * OpenAI API 설정 정보, 채팅 메시지 리스트를 담은 MultiChatRequest 객체를 생성합니다.
      *
-     * @param multiChatMessages 사용자와 시스템 간의 채팅 메시지 리스트
+     * @param multiChatMessageList 사용자와 시스템 간의 채팅 메시지 리스트
      * @return OpenAI API 요청을 위한 MultiChatRequest 객체
      */
-    private MultiChatRequest makeMultiChatRequest(List<MultiChatMessage> multiChatMessages) {
+    private MultiChatRequest makeMultiChatRequest(List<MultiChatMessage> multiChatMessageList) {
         return new MultiChatRequest(
                 openAIProperties.getModel(),
-                multiChatMessages,
+                multiChatMessageList,
                 openAIProperties.getMaxTokens(),
                 openAIProperties.getTemperature(),
                 openAIProperties.getTopP()
@@ -141,16 +143,6 @@ public class ChatServiceImpl implements ChatService {
         headers.setContentType(MediaType.parseMediaType("application/json; charset=UTF-8"));
         headers.add("Authorization", "Bearer " + openAIProperties.getApiKey());
         return new HttpEntity<>(multiChatRequest, headers);
-    }
-
-    /**
-     * OpenAI API에 요청을 보내고 응답을 반환합니다.
-     *
-     * @param httpEntity Http 요청 객체
-     * @return OpenAI API에서 받은 응답을 파싱하여 만든 MultiChatResponse 객체
-     */
-    private MultiChatResponse getResponse(HttpEntity<?> httpEntity) {
-        return restTemplate.postForEntity(openAIProperties.getApiURL(), httpEntity, MultiChatResponse.class).getBody();
     }
 
 
