208133bfe36af63a9683eacf67a547f9a92a1318
김다윤 <dy.jo.kim@gmail.com>
2025-02-12 23:00:35 +0900

    Fix: 채팅 시작할 때 동화 상세를 동화 서비스에서 조회

--- chat/src/main/java/com/tosi/chat/service/ChatServiceImpl.java
+++ chat/src/main/java/com/tosi/chat/service/ChatServiceImpl.java
@@ -1,10 +1,12 @@
 package com.tosi.chat.service;
 
-import com.tosi.chat.common.config.ChatGptProperties;
-import com.tosi.chat.common.exception.CustomException;
-import com.tosi.chat.common.exception.ExceptionCode;
-import com.tosi.chat.dto.*;
-import com.tosi.chat.repository.TaleDetailDtoRedisRepository;
+import com.tosi.chat.config.ChatGptProperties;
+import com.tosi.chat.dto.ChatFinalRequestDto;
+import com.tosi.chat.dto.ChatInitRequestDto;
+import com.tosi.chat.dto.ChatRequestDto;
+import com.tosi.chat.exception.ExceptionCode;
+import com.tosi.common.cache.TaleDetailCacheDto;
+import com.tosi.common.exception.CustomException;
 import io.github.flashvayne.chatgpt.dto.chat.MultiChatMessage;
 import io.github.flashvayne.chatgpt.dto.chat.MultiChatRequest;
 import io.github.flashvayne.chatgpt.dto.chat.MultiChatResponse;
@@ -27,10 +29,8 @@ import java.util.List;
 public class ChatServiceImpl implements ChatService {
     private static final String ROLE_USER = "user";
     private static final String ROLE_SYSTEM = "system";
-    private static final String TALE_CACHE_PREFIX = "taleDetailCache::";
     private final ChatGptProperties chatGptProperties;
     private final RestTemplate restTemplate;
-    private final TaleDetailDtoRedisRepository taleDetailDtoRedisRepository;
     @Value("${openai.api-key}")
     private String apiKey;
     @Value("${service.tale.url}")
@@ -87,7 +87,7 @@ public class ChatServiceImpl implements ChatService {
     }
 
     /**
-     * Redis에서 동화 정보 캐시를 먼저 확인하고, 없으면 Tale 서비스에서 동화 정보를 가져옵니다.
+     * Tale 서비스에서 해당 동화 ID의 동화 상세를 가져옵니다.
      * 가져온 동화 정보를 바탕으로 채팅에 사용할 초기 프롬프트를 생성합니다.
      *
      * @param chatInitRequestDto 사용자, 동화 정보가 담긴 ChatInitRequest 객체
@@ -95,10 +95,8 @@ public class ChatServiceImpl implements ChatService {
      */
     private String makeChatInitPrompt(ChatInitRequestDto chatInitRequestDto) {
         Long taleId = chatInitRequestDto.getTaleId();
-        TaleDetailDto taleDetailDto = taleDetailDtoRedisRepository.findById(TALE_CACHE_PREFIX + taleId)
-                .orElse(restTemplate.getForObject(taleURL + "/content/" + taleId, TaleDetailDto.class));
-
-        return chatInitRequestDto.getChatInitRequestDto(taleDetailDto.getTitle(), taleDetailDto.getContent());
+        TaleDetailCacheDto taleDetailCacheDto = restTemplate.getForObject(taleURL + "/content/" + taleId, TaleDetailCacheDto.class);
+        return chatInitRequestDto.getChatInitRequestDto(taleDetailCacheDto.getTitle(), taleDetailCacheDto.getContent());
     }
 
     /**
